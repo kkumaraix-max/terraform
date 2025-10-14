404da6d2150316ef7489421dd32680abfe7a1aaa
kathojuanil <anilkathoju@gmail.com>
2025-10-14 17:39:57 +0530

    terraform

--- variable/ec2.tf
+++ variable/ec2.tf
@@ -1,29 +1,29 @@
 resource "aws_instance" "terraform" {
     ami = var.ami_id
     instance_type = var.instance_type
-    vpc_security_group_ids = [aws_security_group.allow_all289.id]
+    vpc_security_group_ids = [aws_security_group.accept_all.id]
     tags = var.tags
 }
 
-resource "aws_security_group" "allow_all289" {
-  name   = "allow-all289"
+resource "aws_security_group" "accept_all" {
+  name   = var.security_group
 
   egress {
-    from_port        = 0 # from port 0 to to port 0 means all ports
-    to_port          = 0 
-    protocol         = "-1" # -1 means all protocols
-    cidr_blocks      = ["0.0.0.0/0"] # internet
+    from_port        = var.egress_from_port
+    to_port          = var.egress_to_port
+    protocol         = var.protocol # -1 means all protocols
+    cidr_blocks      = var.cidr_blocks # internet
   }
 
   ingress {
-    from_port        = 0 # from port 0 to to port 0 means all ports
-    to_port          = 0 
-    protocol         = "-1" # -1 means all protocols
-    cidr_blocks      = ["0.0.0.0/0"] # internet
+    from_port        = var.ingress_from_port # from port 0 to to port 0 means all ports
+    to_port          = var.ingress_to_port
+    protocol         = var.protocol # -1 means all protocols
+    cidr_blocks      = var.cidr_blocks # internet
   }
 
   tags = {
-    Name = "allow-all289"
+    Name = var.security_group
   }
 
 }
--- variable/variables.tf
+++ variable/variables.tf
@@ -15,7 +15,42 @@ variable "tags" {
         Name = "mongodb"
         Terraform = "true"
         Project = "JoinDevOps"
+        Environment = "Production"
         }
-
-    
 }
+ variable "security_group" {
+    type = string
+    default = "allowallports1"
+    description = "security group for instance"  
+
+ }
+
+ variable "cidr_blocks" {
+     type = list
+     default = ["0.0.0.0/0"]
+
+ }
+
+ variable "ingress_from_port" {
+    default = 0
+
+ }
+ variable "ingress_to_port" { 
+    default = 0
+
+ }
+
+ variable "egress_from_port" {
+    default = 0
+    
+ }
+  variable "egress_to_port" {
+    type = number
+    default = 0
+    
+ }
+
+ variable "protocol" {
+    type = string
+    default = "-1"
+ }
