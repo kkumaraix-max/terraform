9ec71c9133fd6b4abe88c8655690cb158b4289f5
kathojuanil <anilkathoju@gmail.com>
2025-10-14 21:29:42 +0530

    session-roles

---
+++ countbasedloop/varaible.tf
@@ -0,0 +1,12 @@
+variable "instances" {
+
+    default = [ "mongodb", "redis", "mysql", "rabbitmq" ]
+}
+
+variable "zone_id"{
+    default = "Z096778411CYC46C0VA5Q"
+}
+
+variable "domain_name" {
+    default = "anilkathoju.space"
+}
